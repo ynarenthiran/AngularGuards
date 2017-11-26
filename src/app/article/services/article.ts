eff0bf2fffd8c171a30bdddb70d899f31b300ea8
Y Narenthiran <narenthiran4019@gmail.com>
2017-11-26 07:43:53 +0530

    Intial Commit
    Signed-off-by: Y Narenthiran <narenthiran4019@gmail.com>

---
+++ src/app/article/services/article.ts
@@ -0,0 +1,5 @@
+export class Article { 
+	constructor(public articleId:number, public title:string, public category:string) {
+	}
+}
+    
---
+++ src/app/authentication/services/auth.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+
+import { User } from './user';
+
+const USERS = [
+  new User(1, 'bj', 'bj123', 'ADMIN'),
+  new User(2, 'nnn', 'n123', 'USER')
+];
+let usersObservable = Observable.of(USERS);
+
+@Injectable()
+export class AuthService { 
+    private redirectUrl: string = '/';
+	private loginUrl: string = '/login';
+	private isloggedIn: boolean = false;
+	private loggedInUser: User;
+	getAllUsers(): Observable<User[]> {
+	    return usersObservable;
+	}
+	isUserAuthenticated(username: string, password:string): Observable<boolean> {
+        return this.getAllUsers()
+            .map(users => {
+				let user = 	users.find(user => (user.username === username) && (user.password === password));
+                if(user) {
+					this.isloggedIn = true;
+					this.loggedInUser = user;
+				} else {
+					this.isloggedIn = false;
+				}
+				return this.isloggedIn; 	
+			});
+    }	
+	isUserLoggedIn(): boolean {
+		return this.isloggedIn;
+	}
+	getRedirectUrl(): string {
+		return this.redirectUrl;
+	}
+	setRedirectUrl(url: string): void {
+		this.redirectUrl = url;
+	}
+	getLoginUrl(): string {
+		return this.loginUrl;
+	}
+	getLoggedInUser(): User {
+		return this.loggedInUser;
+	}
+	logoutUser(): void{
+		this.isloggedIn = false;
+	}
+}
